6c74f0496ef1abed58abae45af66e61c957339bd
Gaaraly <1205913452@qq.com>
2022-06-05 21:03:16 +0800

    repo init

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+  ],
+  experimental: {
+    reactivityTransform: true,
+    viteNode: false,
+  },
+  unocss: {
+    preflight: true,
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+  typescript:{
+    shim: false
+  }
+})
