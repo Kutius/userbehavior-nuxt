5eb13483e70fc9c7985188c921160f7ccadf3ea7
Gaaraly <1205913452@qq.com>
2022-06-07 14:26:53 +0800

    chore:navi dev...

--- components/Logos.vue
+++ components/Logos.vue
@@ -1,8 +1,10 @@
 <template>
 	<div inline-flex text-2xl font-300 cursor-default items-center>
-		<div flex children:mx-auto items-center>
-			<img w-12 h-12 inline-block src="/nuxt.png" />
-			<span text-green5 mx-1 font-mono>Nuxt3</span>
-		</div>
+		<nuxt-link to="/">
+			<div flex children:mx-auto items-center>
+				<img w-11 h-11 inline-block src="/nuxt.png" />
+				<span text-green5 mx-1 font-mono>Nuxt3</span>
+			</div>
+		</nuxt-link>
 	</div>
 </template>
---
+++ components/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+	<!-- <div text="xl gray4" m-5 flex justify-center gap-3>
+		<NuxtLink i-carbon-campsite to="/" />
+		<a i-carbon-logo-github href="https://github.com/antfu/vitesse-nuxt3" target="_blank" />
+		<DarkToggle />
+	</div> -->
+	<header h-18>
+		<div flex justify-between items-center h-full px-6 mx-auto max-w-7xl>
+			<Logos />
+			<div>OPTIONS</div>
+			<Operations />
+		</div>
+	</header>
+</template>
