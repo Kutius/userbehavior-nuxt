c73837f2468d1cf4b93e7f6dcc17728fb4593715
Gaaraly <1205913452@qq.com>
2022-08-14 19:18:12 +0800

    feat: processing

---
+++ components/processing/InputEntry.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const host = ref('')
+const files = ref('')
+
+const go = () => {}
+</script>
+
+<template>
+  <div font-sans>
+    <div flex="~ col" items-center gap6 py-4>
+      <input
+        id="input"
+        v-model="host"
+        placeholder="Host IP"
+        type="text"
+        autocomplete="off"
+        p="x-4 y-2"
+        w="250px"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
+        @keydown.enter="go"
+      >
+      <input
+        id="input"
+        v-model="files"
+        placeholder="The name of File"
+        type="text"
+        autocomplete="off"
+        p="x-4 y-2"
+        w="250px"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
+        @keydown.enter="go"
+      >
+    </div>
+    <div>
+      <button m-3 text-sm btn :disabled="!host" @click="go">
+        Upload
+      </button>
+      <button m-3 text-sm btn :disabled="!host" @click="go">
+        Save
+      </button>
+      <button m-3 text-sm btn :disabled="!host" @click="go">
+        Analyze
+      </button>
+    </div>
+  </div>
+</template>
---
+++ pages/processing-log.vue
@@ -0,0 +1,16 @@
+<script lang='ts' setup>
+
+</script>
+
+<template>
+  <div flex="~ col" justify-center lg:px-6 px-4 py-4>
+    <div text-5xl>
+      <div i-carbon:cloud-ceiling inline-block />
+    </div>
+    <p>
+      <em text-sm op75>Selected the Host and Files</em>
+    </p>
+    <ProcessingInputEntry />
+  </div>
+</template>
+
